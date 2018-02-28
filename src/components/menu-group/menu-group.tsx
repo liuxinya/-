f4d4564fa95968f25d72db797b009f6beb0f2ead
liuxinya <1164002431@qq.com>
2018-02-28 22:38:29 +0800

    修复

--- src/components/menu-group/menu-group.tsx
+++ src/components/menu-group/menu-group.tsx
@@ -12,25 +12,25 @@ export class MenuGroup extends Vue {
   @Prop() menuGroupData: any[]; // 当前这一组的对象
   @Prop() menuData: any[];   // 顶级对象
   itmeClick(item, index) {
-    // // console.log(item)
+    console.log(item)
     // console.log(index);
-    // console.log()
     // console.log(this.$refs.childNode)
     // if(this.$refs.childNode[index]){
-    //   // this.$refs.childNode[index].style.height='0px'
-    //   console.log(this.$refs.childNode[index])
-    //   this.$refs.childNode[index].style.height='48px'
-    // }
-    item.open = !item.open;
-    // // 当前点击项有子节点的话 只控制该节点的关闭和打开
-    if(item.children) return;
+      //   // this.$refs.childNode[index].style.height='0px'
+      //   console.log(this.$refs.childNode[index])
+      //   this.$refs.childNode[index].style.height='48px'
+      // }
+      item.open = !item.open;
+      console.log(this.menuData)
+      // // 当前点击项有子节点的话 只控制该节点的关闭和打开
+      if(item.children) return;
     // 循环判断里层有没有children节点 有节点就将所有active取消
     this.loop(this.menuData)
     item.active = true;
   }
-  loop(obj) {
-    obj.forEach((v ,i) => {
-      v.active = false 
+  loop(arr) {
+    arr.forEach((v ,i) => {
+      v.active = false;
       if(v.children) {
         this.loop(v.children);
       }
@@ -47,12 +47,20 @@ export class MenuGroup extends Vue {
     // console.log(this.menuData)
     // 给所有menuGroupData添加一个新属性 open 用于控制每个父节点的点击状态
     // 给所有menuGroupData添加一个新属性 active 用于控制每个父节点的点击状态
-    if(this.menuGroupData) {
-      this.menuGroupData.forEach((item, index) => {
-        Vue.set(item, 'open', true);
-        Vue.set(item, 'active', false);
-      })
-    }
+    // if(this.menuGroupData) {
+    //   this.menuGroupData.forEach((item, index) => {
+    //     Vue.set(item, 'open', true);
+    //     Vue.set(item, 'active', false);
+    //   })
+    // }
+    this.loopAddAttr(this.menuData);
+  }
+  loopAddAttr(arr){
+    arr.forEach((v) => {
+      Vue.set(v, 'open', true);
+      Vue.set(v, 'active', false);
+      if(v.children) this.loopAddAttr(v.children)
+    })
   }
   mounted() {
     // console.log(document.querySelectorAll('.submenu'))
